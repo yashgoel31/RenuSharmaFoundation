932b47cb36e4f608172bbb96cc1c90e529c06475
lokeshbhaskar <lokeshbhaskar28@gmail.com>
2025-06-19 08:11:39 +0530

    Add : WhyUsInternship in Main Internship page

--- src/pages/Internships.jsx
+++ src/pages/Internships.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import TopNavbar_Internship from '../components/TopNavbar_Internship'
 import Hero_Internship from '../components/Hero_Internship'
 import HowItWorks_Internship from '../components/HowItWorks_Internship'
+import WhyUsInternship from '../components/WhyUsInternship'
 
 const Internships = () => {
   return (
     <div>
       <TopNavbar_Internship />
       <Hero_Internship />
+      < WhyUsInternship />
       <HowItWorks_Internship />
     </div>
   )
