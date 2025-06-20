f0f58ec242470b5dec7279961fe6ac0f7808a2ab
lokeshbhaskar <lokeshbhaskar28@gmail.com>
2025-06-20 11:51:34 +0530

    fix index.css

--- src/pages/Internships.jsx
+++ src/pages/Internships.jsx
@@ -1,29 +1,27 @@
-import React from 'react'
-import TopNavbar_Internship from '../components/TopNavbar_Internship'
-import Hero_Internship from '../components/Hero_Internship'
-// import HeroSection from '../components/HeroSection'
-import HowItWorks_Internship from '../components/HowItWorks_Internship'
-import WhyUsInternship from '../components/WhyUsInternship'
-<<<<<<< HEAD
-=======
-import Alumni_Internship from '../components/Alumni_Internship'
->>>>>>> 9e44a279732b6e834b308a4b2aa7cddc8c0c99ca
+import React from "react";
+import TopNavbar_Internship from "../components/TopNavbar_Internship";
+import Hero_Internship from "../components/Hero_Internship";
+import HeroSection from "../components/HeroSection";
+import HowItWorks_Internship from "../components/HowItWorks_Internship";
+import WhyUsInternship from "../components/WhyUsInternship";
+import HandsOnProjectSection_Internships from "../components/HandsOnProjectSection_Internships";
+import Alumni_Internship from "../components/Alumni_Internship";
+import Footer_Internship from "../components/Footer_Internship";
+import NextCohortSection_Internship from "../components/NextCohortSection_Internship";
 
 const Internships = () => {
   return (
     <div>
       <TopNavbar_Internship />
       <Hero_Internship />
-<<<<<<< HEAD
-      < WhyUsInternship />
-=======
-      {/* <HeroSection /> */}
       <WhyUsInternship />
->>>>>>> 9e44a279732b6e834b308a4b2aa7cddc8c0c99ca
       <HowItWorks_Internship />
+      <HandsOnProjectSection_Internships />
       <Alumni_Internship />
+      <NextCohortSection_Internship />
+      <Footer_Internship />
     </div>
-  )
-}
+  );
+};
 
-export default Internships
+export default Internships;
