94bb52dea6a72d8905062d5828bb2f8dc52053af
yashgoel31 <yashgoelbsr@gmail.com>
2025-06-23 21:42:25 +0530

    commit

--- src/pages/Internships.jsx
+++ src/pages/Internships.jsx
@@ -3,9 +3,11 @@ import TopNavbar_Internship from '../components/TopNavbar_Internship'
 import Hero_Internship from '../components/Hero_Internship'
 // import HeroSection from '../components/HeroSection'
 import HowItWorks_Internship from '../components/HowItWorks_Internship'
+import HandsOnProjectSection_Internships from '../components/HandsOnProjectSection_Internships'
 import WhyUsInternship from '../components/WhyUsInternship'
 
 import Alumni_Internship from '../components/Alumni_Internship'
+import NextCohortSection_Internship from '../components/NextCohortSection_Internship'
 
 import Client_Showcase_Internship from '../components/Client_Showcase_Internship'
 import Faq_Internship from '../components/Faq_Internship'
@@ -19,8 +21,10 @@ const Internships = () => {
       <WhyUsInternship />
       {/* <HeroSection /> */}
       <HowItWorks_Internship />
+      <HandsOnProjectSection_Internships />
       <Alumni_Internship />
       <Client_Showcase_Internship />
+      <NextCohortSection_Internship />
       <Faq_Internship />
     </div>
   );
